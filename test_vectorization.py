bb3a9d2275db102e87941b711102f09d443741c8
Abraham Oosterhuis <rendord@gmail.com>
2026-01-22 23:25:03 +0100

    explored problem space, wrote down ideas for testing software, experimented with dilation and erosion

--- test_vectorization.py
+++ test_vectorization.py
@@ -23,15 +23,29 @@ def rotate_image(img: np.ndarray, angle_deg: float) -> np.ndarray:
 
 
 panelPath = Path("manga_scans/jp2/BLAME!_Master_Edition_v01__0000.jp2")
+panelPath_2 = Path("manga_scans/jp2/BLAME!_Master_Edition_v01__0000.jp2")
 
 img = cv2.imread(str(panelPath), cv2.IMREAD_GRAYSCALE)
+img_2 = cv2.imread(str(panelPath_2), cv2.IMREAD_GRAYSCALE)
+
+
+#KERNEL
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
 # Rotate first (example: slight deskew)
-retval, bw_img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+retval, bw_img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+
+ink_img = 255 - bw_img
+
+#ink_open = cv2.morphologyEx(ink_img, cv2.MORPH_OPEN, kernel, iterations=1)
+ink_clean = cv2.morphologyEx(ink_img, cv2.MORPH_CLOSE, kernel, iterations=2)
+
 
 scale = 0.3
 h, w = img.shape[:2]
-resized = cv2.resize(bw_img, (int(w * scale), int(h * scale)))
+resized = cv2.resize(ink_clean, (int(w * scale), int(h * scale)))
+resized_2 = cv2.resize(img_2, (int(w * scale), int(h * scale)))
 
 #rotate using custom function rotates just the image while keeping hxw of original array
 img_rotated = rotate_image(resized, angle_deg=90.0)
@@ -41,10 +55,30 @@ img_rotated_cv = cv2.rotate(resized, cv2.ROTATE_90_CLOCKWISE)
 
 
 
+#TODO build a comparison display that holds several images side by to see modification steps
+# Make a panel class that holds a list of operations
+# When rendering we display the steps side by side in the order we applied them
+# what we can do is create overrided methods for each CV2 operation
+# we track the image in every state and then we call a display method that shows them
+# in the same window
+
+#TODO build a little UI to flick through manga pages and apply operations interactively
+#we need some kind of state machine for this and a keylistener
+# we want to be able to apply operations and split the window so we can perform
+# a couple operations and then split at that stage and then continue applying operations to the second panel
+
+
+#PROBLEM: when dilating and eroding the lines that determine the shape of an object
+# fade away and the object becomes one with the background
+# we need to find a way to preserve these lines while cleaning up noise
+# manga panels and 2D art relies heavily on suggestion to define shapes and forms
+# so we need to find a way to complete lines and make them thicker
 
 print(img.dtype, img.shape, img.min(), img.max())
 print("retval threshhold used:", retval)
 
-cv2.imshow("Panel", img_rotated_cv)
+cv2.imshow("Panel", resized)
+
+cv2.imshow("Unaffected", resized_2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
