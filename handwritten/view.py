35768389d149d35611c21fce474f2b7ead51991c
Abraham Oosterhuis <rendord@gmail.com>
2026-02-07 13:08:24 +0100

    added function to convert from OpenCV matlike to QT image

--- handwritten/view.py
+++ handwritten/view.py
@@ -1,6 +1,9 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QSizePolicy
 from PyQt6.QtCore import Qt
-
+from PyQt6.QtGui import QImage
+import cv2
+from cv2.typing import MatLike
+import np
 
 class ColorSquare(QWidget):
     def __init__(self, color: str):
@@ -32,6 +35,28 @@ grid.addWidget(blue, 0, 2, 2, 1)
 
 #grid.addWidget(red, 2, 1, 1, 3)
 
+def returnQImage(img: MatLike) -> QImage:
+    validateImg()
+    if img.ndim == 2:
+        rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    if img.shape[2] == 3:
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    if img.shape[2] == 4:
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+
+    if rgb:
+        return QImage(rgb.data, rgb.shape[0], rgb.shape[1], rgb.strides[0], QImage.Format_RGB888).copy()
+    else:
+        #TODO implement placeholder image or similar
+        return QImage()
+
+def validateImg(img: MatLike):
+    if img.dtype != np.uint8:
+        raise TypeError("Expected uint8 image")
+    if img.ndim not in (2, 3): 
+        raise ValueError("Not an image") 
+    if img.ndim == 3 and img.shape[2] not in (3, 4): 
+        raise ValueError("Unsupported channel count")
 
 
 
